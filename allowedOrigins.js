295ebbba5798179a162a95bf0f73af6eb1f5902a
ShadReza007 <63732556+ShadReza007@users.noreply.github.com>
2024-09-13 20:12:45 +0530

    updated

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
      'http://localhost:3000',
      'http://localhost:3001',
      'https://shopvibe.onrender.com',
+     'https://66e350794d7336363cabdbf9--willowy-yeot-3fec2d.netlify.app/',
      'https://66e44dd26e5cdc007a430375--rad-sorbet-0f28d7.netlify.app/'
 ]
 
